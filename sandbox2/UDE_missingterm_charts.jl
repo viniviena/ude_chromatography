78bb5a6d3a02668801e7d10752a1578e09530090
vinicius <vinicius.viena1@gmail.com>
2023-02-03 13:48:39 +0000

    chart langmuir missing term

--- sandbox2/UDE_missingterm_charts.jl
+++ sandbox2/UDE_missingterm_charts.jl
@@ -1,8 +1,13 @@
 using Pkg
 Pkg.activate(".")
-Pkg.instantiate()
 
-using PGFPlots
+using Plots
+using TikzPictures
+pgfplotsx()
 using MAT
 using DelimitedFiles
 
+data = readdlm("sparse_reg_data/stats_lang_improved_kldf.csv", ',')
+
+plot(randn(19))
+savefig(fig, "test.pdf")
